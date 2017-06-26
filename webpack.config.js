405608742b2a8f08efa6a415ac997535fcba9e0b
Stephen Wisner <spwisner@gmail.com>
2017-06-26 17:33:16 -0400

    Updated Webpack
    Webpack Updated to include proxy to API

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,43 @@
 'use strict';
 
+const webpack = require('webpack');
+
 module.exports = {
-  entry: './src/App.jsx',
+  entry: {
+    app: './src/App.jsx',
+    vendor: [
+      'react', 'react-dom', 'react-router', 'react-bootstrap', 'react-router-bootstrap',
+      'isomorphic-fetch', 'react-select'
+    ],
+  },
   output: {
     path: __dirname + '/static',
-    filename: 'app.bundle.js'
+    filename: 'app.bundle.js',
   },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity
+    }),
+  ],
   module: {
     loaders: [
       {
         test: /\.jsx$/,
         loader: 'babel-loader',
         query: {
-          presets: ['react','es2015']
-        }
+          presets: ['react', 'es2015'],
+        },
       },
-    ]
-  }
+    ],
+  },
+  devServer: {
+    port: 4741,
+    contentBase: 'static',
+    proxy: {
+      '**': {
+        target: 'http://localhost:3000',
+      },
+    },
+  },
 };
