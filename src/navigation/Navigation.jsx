f8976c7dd243306368af1ad5f5975eadf95a5032
Stephen Wisner <spwisner@gmail.com>
2017-06-28 12:26:35 -0400

    Updated search bar place holder

--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -64,7 +64,7 @@ export default class Navigation extends React.Component {
             </ul>
             <form className="navbar-form navbar-right" name="searchSnippets" role="search" onSubmit={this.handleSubmit}>
               <div className="form-group">
-                <input type="text" name="searchInput" className="form-control search-input" placeholder="Search" />
+                <input type="text" name="searchInput" className="form-control search-input" placeholder="Search By Title" />
               </div>
               <button type="submit" className="btn btn-default">Submit</button>
             </form>
