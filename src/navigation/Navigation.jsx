87c05d71bd8426bd972b0ce6551d32bd9fbc3afd
Stephen Wisner <spwisner@gmail.com>
2017-06-27 22:11:40 -0400

    Updated Navigation Styling

--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -40,25 +40,27 @@ export default class Navigation extends React.Component {
 
   render() {
     return (
-      <nav className="navbar navbar-inverse">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <span className="navbar-brand" href="#" onClick={this.snippetsOnClick}>Save Snippet</span>
+      <div>
+        <nav className="navbar navbar-inverse">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <span className="navbar-brand" href="#" onClick={this.snippetsOnClick}>Save Snippet</span>
+            </div>
+            <ul className="nav navbar-nav">
+              <li className={this.activeClass(this.props.snippetsState)} onClick={this.snippetsOnClick}><a href="#">Home</a></li>
+              <li className={this.activeClass(this.props.createState)} onClick={this.createOnClick}><a href="#">Create</a></li>
+            </ul>
+            <form className="navbar-form navbar-right navbar-form">
+              <div className="form-group">
+                <input type="text" className="form-control" placeholder="Search" />
+                </div>
+                <button className="btn btn-default" type="submit">
+                  <i className="glyphicon glyphicon-search"></i>
+                </button>
+            </form>
           </div>
-          <ul className="nav navbar-nav">
-            <li className={this.activeClass(this.props.snippetsState)} onClick={this.snippetsOnClick}><a href="#">Home</a></li>
-            <li className={this.activeClass(this.props.createState)} onClick={this.createOnClick}><a href="#">Create</a></li>
-          </ul>
-          <form className="navbar-form navbar-right navbar-form">
-            <div className="form-group">
-              <input type="text" className="form-control" placeholder="Search" />
-              </div>
-              <button className="btn btn-default" type="submit">
-                <i className="glyphicon glyphicon-search"></i>
-              </button>
-          </form>
-        </div>
-      </nav>
+        </nav>
+      </div>
     )
   }
 };
