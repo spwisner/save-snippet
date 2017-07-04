6d95ac6d89442d85d1625971c537fdbae4683d56
Stephen Wisner <spwisner@gmail.com>
2017-07-04 17:44:21 -0400

    Deleted Duplicate Files & Sign-In Request Made
    - Updated sign-in to send login request using fetch
    
    Next Step: store user token

--- assets/scripts/credentials/prelogin/SignIn.jsx
+++ assets/scripts/credentials/prelogin/SignIn.jsx
@@ -2,19 +2,55 @@
 
 import React from 'react';
 
+const config = require('../../config');
+const api = config.apiOrigins.production;
+
 export default class SignIn extends React.Component {
+  constructor() {
+    super();
+
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+  }
+
+  signInRequest(data) {
+    fetch(`${api}/sign-in`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .catch(err => {
+      alert("Error in sending data to server: " + err.message);
+    });
+  }
+
+
+  handleLoginSubmit(event) {
+    event.preventDefault();
+    const form = document.forms.signInForm;
+
+    const data = {
+      credentials: {
+        email: form.email.value,
+        password: form.password.value
+      }
+    }
+
+    this.signInRequest(data);
+  }
+
   render() {
     return (
       <div className="container-fluid">
         <h2>Sign-In</h2>
-        <form className="form login-form">
+        <form className="form login-form" name="signInForm" onSubmit={this.handleLoginSubmit}>
           <div className="form-group">
             <label>Email:</label>
-            <input type="text" className="form-control" id="usr" />
+            <input className="form-control" type="text" name="email" />
           </div>
           <div className="form-group">
             <label>Password:</label>
-            <input type="password" className="form-control" id="pwd" />
+            <input className="form-control" type="password" name="password" />
           </div>
           <input type="submit" id="btnLogin" className="btn btn-block btn-success btn-lg" value="Sign-In" />
           <div className="register-bg">
