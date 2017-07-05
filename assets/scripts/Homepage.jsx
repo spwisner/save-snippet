61a50eefd2e1c024d4ceae5011621442bb33c52c
Stephen Wisner <spwisner@gmail.com>
2017-07-05 13:27:39 -0400

    Created placeholder homepage

---
+++ assets/scripts/Homepage.jsx
@@ -0,0 +1,17 @@
+'use strict';
+
+import React from 'React';
+
+export default class Homepage extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Save Snippet</h1>
+        <p>
+          Save Snippet is an application in which a user can store code for
+        future reference
+        </p>
+      </div>
+    )
+  }
+}
