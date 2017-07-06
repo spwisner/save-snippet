8934b89d7345a971fdd07db6d6584d1813b34a8d
Stephen Wisner <spwisner@gmail.com>
2017-07-06 02:20:02 -0400

    Prep API Origin for deployment

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,8 +2,8 @@
 
 const config = {
   apiOrigins: {
-    // production: 'https://save-snippet-api.herokuapp.com'
-    production: 'http://localhost:4741'
+    production: 'https://save-snippet-api.herokuapp.com'
+    // production: 'http://localhost:4741'
   },
 };
 
